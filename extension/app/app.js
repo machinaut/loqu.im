561ae7c53730e58ea420bee6450671f20e6db459
Alex Ray <a@loqu.im>
2012-04-23 12:34:30 -0400

    incremental, working on live rendering

--- extension/app/app.js
+++ extension/app/app.js
@@ -1,5 +1,67 @@
 (function() {
+  var render, tst;
 
-  console.log("I'm ready!");
+  tst = "# Title Here\ntext here";
+
+  render = function(text) {
+    var line, out, tag, _i, _len, _ref;
+    out = '';
+    _ref = text.split('\n');
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      line = _ref[_i];
+      tag = '';
+      if (line[0] === '#') {
+        if (line[1] === '#') {
+          if (line[2] === '#') {
+            if (line[3] === '#') {
+              if (line[4] === '#') {
+                if (line[5] === '#') {
+                  tag = 'h6';
+                } else {
+                  tag = 'h5';
+                }
+              } else {
+                tag = 'h4';
+              }
+            } else {
+              tag = 'h3';
+            }
+          } else {
+            tag = 'h2';
+          }
+        } else {
+          tag = 'h1';
+        }
+      } else {
+        tag = 'p';
+      }
+      out += "<" + tag + ">" + line + "</" + tag + ">";
+    }
+    return out;
+  };
+
+  $(function() {
+    $('#app').html(render(tst)).attr('contenteditable', 'true').live('focus', function() {
+      var $this;
+      $this = $(this);
+      $this.data('before', $this.html());
+      return $this;
+    }).live('blur keyup paste', function() {
+      var $this;
+      $this = $(this);
+      if ($this.data('before') !== $this.html()) {
+        $this.data('before', $this.html());
+        $this.trigger('change');
+      }
+      return $this;
+    }).live('change', function() {
+      var $this;
+      $this = $(this);
+      $this.html(render($this.html()));
+      return console.log('changing!');
+    });
+    $('#react').html(render(tst));
+    return console.log("I'm ready!");
+  });
 
 }).call(this);
--- source/coffee/app.coffee
+++ source/coffee/app.coffee
@@ -1,1 +1,47 @@
-console.log "I'm ready!"
+tst = "# Title Here\ntext here"
+
+render = (text) ->
+  out = ''
+  for line in text.split('\n')
+    tag = ''
+    # LOOK AT THE PRETTY ARROW! I CAN HAZ GOOD CODEZ
+    if line[0] is '#'
+      if line[1] is '#'
+        if line[2] is '#'
+          if line[3] is '#'
+            if line[4] is '#'
+              if line[5] is '#' then tag = 'h6' else tag = 'h5'
+            else tag = 'h4'
+          else tag = 'h3'
+        else tag = 'h2'
+      else tag = 'h1'
+    else tag = 'p'
+    out += "<#{tag}>#{line}</#{tag}>"
+  return out
+
+#explode = (t) ->
+
+# possibly just reparse the whole document every freakign time.
+# that could work
+$ ->
+  $('#app')
+    .html(render tst)
+    .attr('contenteditable','true')
+    .live 'focus', ->
+      $this = $(this)
+      $this.data 'before', $this.html()
+      return $this
+    .live 'blur keyup paste', ->
+      $this = $(this)
+      if $this.data('before') isnt $this.html()
+        $this.data 'before', $this.html()
+        $this.trigger('change')
+      return $this
+    .live 'change', ->
+      $this = $(this)
+      $this.html render $this.html()
+      console.log 'changing!'
+  $('#react')
+    .html render tst
+  console.log "I'm ready!"
+
